7ba8ad596ac12701780a826e25a82b926a448369
Zachary Thomas Nixon <znixon@cs.arizona.edu>
2016-04-14 16:57:52 -0700

    start of vm

--- phase3.c
+++ phase3.c
@@ -5,10 +5,28 @@
  Zachary Nixon
  */
 
-#include <assert.h>
 #include <phase3.h>
+#include <phase2.h>
+#include <phase1.h>
 #include <string.h>
 #include <libuser.h>
+
+#define VMON 1
+#define VMOFF 0 
+
+#define PAGES 0
+#define FRAMES 1
+#define BLOCKS 2
+#define FFRAMES 3
+#define FBLOCKS 4
+#define SWITCHES 5
+#define FAULTS 6
+#define NEW 7
+#define PINS 8
+#define POUTS 9
+#define REPLACED 10
+
+#define SWAP_DISK_NUM 1
 
 /*
  * Everybody uses the same tag.
@@ -43,6 +61,10 @@ static Process processes[P1_MAXPROC];
 static int numPages = 0;
 static int numFrames = 0;
 
+static int* frameList;
+static P1_Semaphore FLSem;
+
+
 /*
  * Information about page faults.
  */
@@ -57,14 +79,16 @@ int FAULT_SIZE = sizeof(Fault);
 static void *vmRegion = NULL;
 
 P3_VmStats P3_vmStats;
+static P1_Semaphore vmStatsSem;
 
 static int pagerMbox = -1;
 static P1_Semaphore pagerRunning;
-static int numbPagers = -1;
+static int numPagers = -1;
 
 static void CheckPid(int);
 static void CheckMode(void);
 static int CheckInit(void);
+static void ModifyStats(int field,int modifier);
 static void FaultHandler(int type, void *arg);
 static int Pager(void *arg);
 
@@ -75,9 +99,9 @@ void P3_Quit(int pid);
 static int done = 0;
 
 int P3_Startup(void *arg){
+	int status;
 	int pid;
 	Sys_Spawn("P4_Startup", P4_Startup, NULL, 4 * USLOSS_MIN_STACK, 3,&pid);	
-	int status;
 	Sys_Wait(&pid,&status);
 	return 0;
 }
@@ -108,10 +132,9 @@ int P3_VmInit(int mappings, int pages, int frames, int pagers) {
 		return -1;
 	}
 	
-	if(!CheckInit()){
+	if(CheckInit() == VMON){
 		return -2;
-	}
-	
+	}	
 	status = USLOSS_MmuInit(mappings, pages, frames);
 	if (status != USLOSS_MMU_OK) {
 		USLOSS_Console("P3_VmInit: couldn't initialize MMU, status %d\n",
@@ -119,8 +142,6 @@ int P3_VmInit(int mappings, int pages, int frames, int pagers) {
 		USLOSS_Halt(1);
 	}
 	vmRegion = USLOSS_MmuRegion(&tmp);
-	assert(vmRegion != NULL);
-	assert(tmp >= pages);
 	USLOSS_IntVec[USLOSS_MMU_INT] = FaultHandler;
 	for (i = 0; i < P1_MAXPROC; i++) {
 		processes[i].numPages = 0;
@@ -131,19 +152,35 @@ int P3_VmInit(int mappings, int pages, int frames, int pagers) {
 	 */
 	pagerMbox = P2_MboxCreate(P1_MAXPROC,sizeof(Fault));
         pagerRunning = P1_SemCreate(0);
-	numbPagers = pagers;
+	numPagers = pagers;
+	done = 0;
+	vmStatsSem = P1_SemCreate(1);
+	FLSem = P1_SemCreate(1);
 
-	int j;
-	for(i = 0; i < pagers; i++){
-		Sys_Spawn("Pager",Pager,NULL,USLOSS_MIN_STACK,P3_PAGER_PRIORITY,&j);
-		Sys_SemP(pagerRunning);
+	frameList = malloc(sizeof(int) * frames);	
+	for(i = 0; i < frames;i++){
+		frameList[i] = UNUSED;
 	}
+
+	for(i = 0; i < pagers; i++){
+		P1_Fork("Pager",Pager,NULL,USLOSS_MIN_STACK,P3_PAGER_PRIORITY);
+		P1_P(pagerRunning);
+	}
+
 	memset((char *) &P3_vmStats, 0, sizeof(P3_VmStats));
-	P3_vmStats.pages = pages;
-	P3_vmStats.frames = frames;
+	ModifyStats(PAGES,pages);
+	ModifyStats(FRAMES,frames);
+	ModifyStats(FFRAMES,frames);
+	int sector;
+	int numSectors;
+	int tracks;
+	P2_DiskSize(SWAP_DISK_NUM,&sector,&numSectors,&tracks);	
+	ModifyStats(FBLOCKS,(sector * numSectors * tracks) / USLOSS_MmuPageSize());
+	ModifyStats(BLOCKS,(sector * numSectors * tracks) / USLOSS_MmuPageSize());
+	//ModifyStats(BLOCKS,USLOSS_MmuPageSize());
 	numPages = pages;
 	numFrames = frames;
-	return numPages * USLOSS_MmuPageSize();
+	return 0;
 }
 /*
  *----------------------------------------------------------------------
@@ -162,7 +199,7 @@ int P3_VmInit(int mappings, int pages, int frames, int pagers) {
  */
 void P3_VmDestroy(void) {
 	CheckMode();
-	if(CheckInit()){
+	if(CheckInit() == VMOFF){
 		return;
 	}
 	USLOSS_MmuDone();
@@ -172,15 +209,17 @@ void P3_VmDestroy(void) {
 	done = 1;
 	int i;
 	int status;
-	int pid;
 	Fault f;
-	for(i = 0; i < P3_MAX_PAGERS;i++){
-		Sys_MboxCondSend(pagerMbox,&f,&FAULT_SIZE);
-		Sys_Wait(&pid,&status);
+	for(i = 0; i < numPagers;i++){
+		P2_MboxCondSend(pagerMbox,&f,&FAULT_SIZE);
+		P1_Join(&status);
 	}
+
+	free(frameList);
 	/*
 	 * Print vm statistics.
 	 */
+	P1_P(vmStatsSem);
 	USLOSS_Console("P3_vmStats:\n");
 	USLOSS_Console("pages: %d\n", P3_vmStats.pages);
 	USLOSS_Console("frames: %d\n", P3_vmStats.frames);
@@ -193,6 +232,7 @@ void P3_VmDestroy(void) {
 	USLOSS_Console("pageIns: %d\n",P3_vmStats.pageIns);
 	USLOSS_Console("pageOuts: %d\n",P3_vmStats.pageOuts);
 	USLOSS_Console("replcaed: %d\n",P3_vmStats.replaced);
+	P1_V(vmStatsSem);
 }
 
 /*
@@ -210,14 +250,13 @@ void P3_VmDestroy(void) {
  *
  *----------------------------------------------------------------------
  */
-void P3_Fork(pid)
-	int pid; /* New process */
+void P3_Fork(int pid)
 {
 	int i;
 
 	CheckMode();
 	CheckPid(pid);
-	if(CheckInit()){
+	if(CheckInit() == VMOFF){
 		return;
 	}
 	processes[pid].numPages = numPages;
@@ -246,23 +285,34 @@ void P3_Fork(pid)
  *
  *----------------------------------------------------------------------
  */
-void P3_Quit(pid)
-	int pid; {
+void P3_Quit(int pid) {
 	CheckMode();
 	CheckPid(pid);
-        if(CheckInit()){
+        if(CheckInit() == VMOFF){
                 return;
         }
-	assert(processes[pid].numPages > 0);
-	assert(processes[pid].pageTable != NULL);
 
 	/*
 	 * Free any of the process's pages that are on disk and free any page frames the
 	 * process is using.
 	 */
+	
+	int i;
+
+	/*
+	Need to handle pages on the swap disk as well. PART B.
+	*/
+	P1_P(FLSem);
+	for(i = 0; i < numPages;i++){
+		/*If this page has a frame in memory, make it unused.*/
+		if(processes[pid].pageTable[i].state == INCORE){
+			frameList[processes[pid].pageTable[i].frame] = UNUSED;		
+			ModifyStats(FFRAMES,1);
+		}
+	}
+	P1_V(FLSem);
 
 	/* Clean up the page table. */
-
 	free((char *) processes[pid].pageTable);
 	processes[pid].numPages = 0;
 	processes[pid].pageTable = NULL;
@@ -284,11 +334,9 @@ void P3_Quit(pid)
  *
  *----------------------------------------------------------------------
  */
-void P3_Switch(old, new)
-	int old; /* Old (current) process */
-	int new; /* New process */
+void P3_Switch(int old, int new)
 {
-        if(CheckInit()){
+        if(CheckInit() == VMOFF){
                 return;
         }
 	int page;
@@ -298,17 +346,22 @@ void P3_Switch(old, new)
 	CheckPid(old);
 	CheckPid(new);
 
-	P3_vmStats.switches++;
+	if(CheckInit() == VMOFF){
+		return
+	}
+
+	ModifyStats(SWITCHES,1);
 	for (page = 0; page < processes[old].numPages; page++) {
 		/*
 		 * If a page of the old process is in memory then a mapping
 		 * for it must be in the MMU. Remove it.
 		 */
 		if (processes[old].pageTable[page].state == INCORE) {
-			assert(processes[old].pageTable[page].frame != -1);
 			status = USLOSS_MmuUnmap(TAG, page);
 			if (status != USLOSS_MMU_OK) {
 				// report error and abort
+				USLOSS_Console("Unable to un-map page.\n");
+				USLOSS_Halt(1);	
 			}
 		}
 	}
@@ -318,11 +371,11 @@ void P3_Switch(old, new)
 		 * for it to the MMU.
 		 */
 		if (processes[new].pageTable[page].state == INCORE) {
-			assert(processes[new].pageTable[page].frame != -1);
 			status = USLOSS_MmuMap(TAG, page,
 					processes[new].pageTable[page].frame, USLOSS_MMU_PROT_RW);
 			if (status != USLOSS_MMU_OK) {
-				// report error and abort
+				USLOSS_Console("Unable to map page.\n");
+				USLOSS_Halt(1);
 			}
 		}
 	}
@@ -351,24 +404,16 @@ static void FaultHandler(type, arg)
 	int status;
 	Fault fault;
 	int size;
-
-	assert(type == USLOSS_MMU_INT);
 	cause = USLOSS_MmuGetCause();
-	assert(cause == USLOSS_MMU_FAULT);
-	P3_vmStats.faults++;
+	ModifyStats(FAULTS,1);
 	fault.pid = P1_GetPID();
 	fault.addr = arg;
 	fault.mbox = P2_MboxCreate(1, 0);
-	assert(fault.mbox >= 0);
 	size = sizeof(fault);
 	status = P2_MboxSend(pagerMbox, &fault, &size);
-	assert(status == 0);
-	assert(size == sizeof(fault));
 	size = 0;
 	status = P2_MboxReceive(fault.mbox, NULL, &size);
-	assert(status == 0);
 	status = P2_MboxRelease(fault.mbox);
-	assert(status == 0);
 }
 
 /*
@@ -389,13 +434,30 @@ static void FaultHandler(type, arg)
  */
 int Pager(void *arg) {
 	Fault f;
+	P1_V(pagerRunning);
+	int i;
+	int newFrame;
+	int status;
+	void *startVM = USLOSS_MmuRegion(&status);
+	int pageSize = USLOSS_MmuPageSize();
 	while (1) {
-		Sys_MboxReceive(pagerMbox,&f,&FAULT_SIZE);
+		P2_MboxReceive(pagerMbox,&f,&FAULT_SIZE);
 		if(done == 1){
 			break;
 		}
-		/* Wait for fault to occur (receive from pagerMbox) */
 		/* Find a free frame */
+		P1_P(FLSem);
+		newFrame = -1;
+		for(i = 0 ; i < numFrames; i++){
+			if(frameList[i] == UNUSED){
+				ModifyStats(FFRAMES,-1);
+				newFrame = i;
+				frameList[i] = INCORE;
+				status = USLOSS_MmuMap(TAG, page);
+			}
+		}
+		P1_V(FLSem);
+		//USLOSS_MmuPageSize()
 		/* If there isn't one run clock algorithm, write page to disk if necessary */
 		/* Load page into frame from disk or fill with zeros */
 		/* Unblock waiting (faulting) process */
@@ -423,5 +485,37 @@ static void CheckMode(void) {
 }
 
 static int CheckInit(void){
-	return !(pagerMbox == -1);
+	if(pagerMbox == -1){
+		return VMOFF;
+	}
+	return VMON;
+}
+
+static void ModifyStats(int field,int modifier){
+	P1_P(vmStatsSem);
+	switch(field){
+	case PAGES: P3_vmStats.pages += modifier;
+	break;
+	case FRAMES: P3_vmStats.frames += modifier;
+	break;
+	case BLOCKS: P3_vmStats.blocks += modifier;
+	break;
+	case FFRAMES: P3_vmStats.freeFrames += modifier;
+	break;
+	case FBLOCKS: P3_vmStats.freeBlocks += modifier;
+	break;
+	case SWITCHES: P3_vmStats.switches += modifier;
+	break;
+	case FAULTS: P3_vmStats.faults += modifier;
+	break;
+	case NEW: P3_vmStats.new += modifier;
+	break;
+	case PINS: P3_vmStats.pageIns += modifier;
+	break;
+	case POUTS: P3_vmStats.pageOuts += modifier;
+	break;
+	case REPLACED: P3_vmStats.replaced += modifier;
+	break;
+	}
+	P1_V(vmStatsSem);	
 }
